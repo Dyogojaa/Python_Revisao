4aeeb1ebc68a4ae0167c8a9016cd1055511a6fa9
Dyogo <dyogo@gmail.com>
2023-05-13 19:49:50 -0300

    Primeiro commit

---
+++ aula2.py
@@ -0,0 +1,5 @@
+
+# \r\n = CRLF (Carrige Return Line Feed) São caracteres especiais de quebra de linha
+print(11,23, sep="-", end="##\n")
+print(56,98, sep='-', end='\n#')
+print(125,88, sep='-')
---
+++ aula3.py
@@ -0,0 +1,21 @@
+""" 
+    Python = Linguagem de Programação
+    Tipo de Tipagem = Dinâmica / Forte
+    str -> string -> texto
+    String são textos que estão dentro de aspas
+"""
+
+print(1234)
+
+# Aspas simples
+print('Dyogo José Aparecido de Almeida')
+print(1,'"Dyogo José Aparecido de Almeida"')
+# Aspas Duplas
+print("Dyogo José Aparecido de Almeida")
+print(2,"'Dyogo José Aparecido de Almeida'")
+
+# Escape
+print("Dyogo José \"Aparecido de Almeida\"")
+
+# r
+print(r"Dyogo José \"Aparecido de Almeida\"")
