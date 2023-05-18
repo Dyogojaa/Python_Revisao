75d44b4b6a4570a5bd0411850cc2e8e643048b79
Dyogo <dyogo@gmail.com>
2023-05-18 20:38:53 -0300

    Ajuste

--- aula10.py
+++
@@ -1,7 +0,0 @@
-concatenacao = 'Dyogo' + ' '+ 'JAA'  +str(1) #Sinal de + Efetua a Concatenação e Soma
-print(concatenacao)
-
-a_dez_vez = 'A' *10 #Asteristico faz a repetição com string e Multiplicação com numeros
-tres_vezes_luiz  = 3 * 'Dyogo '
-print(a_dez_vez)
-print(tres_vezes_luiz)
--- aula11.py
+++
@@ -1,8 +0,0 @@
-
-# Precedencia dos Valores
-# 1 (n+n) Primerio é executado as operações dentros dos parenteses 
-# 2 ** depois a exponenciação
-# 3 * / // % depois multiplicação, divisão, divisão inteiro, modulo
-# 4 + - por ultima soma e subtração
-conta_1 = 1+1 ** 5 +5 
-print(conta_1)
